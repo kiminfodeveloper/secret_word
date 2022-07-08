3f051ddb17c1b7c9f93952ea4c4a04fd9f11814f
Kevyn Melo <kimdeveloper@hotmail.com>
2022-07-08 13:28:12 -0300

    jogo de palavras secretas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,167 @@
-import logo from './logo.svg';
+//css
 import './App.css';
 
+//react
+import { useCallback, useEffect, useState } from 'react'
+
+//data
+import { wordsList } from './data/words'
+
+//components
+import StartScreen from './data/components/StartScreen';
+import Game from './data/components/Game';
+import GameOver from './data/components/GameOver';
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
+
+//qtde de tentativas.
+const guessesQty = 6
+
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList)
+
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState ([]);
+
+  const [guessedLetters, setGuessedLetters] = useState ([])
+  const [wrongLetters, setWrongLetters] = useState([])
+  const [guesses, setGuesses] = useState(guessesQty)
+  const [score, setScore] = useState(0)
+  
+  const pickWordAndCategory = useCallback(() => {
+    //pick a random category
+    const categories = Object.keys(words);
+    const category = 
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+        //console.log(category);
+
+    //pick a random word
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+        //console.log(word);
+    
+    return { word, category };
+  }, [words]);
+
+  //inicio do jogo
+  const startGame = useCallback(() => {
+    //clear all letters
+    clearLettersStates();
+
+
+    //pick word and pick category
+    const { word, category } = pickWordAndCategory();
+      //console.log(word, category);
+
+    //create an array of letters
+    let wordLetters = word.split("");
+    
+    wordLetters = wordLetters.map((l) => l.toLowerCase())
+      //console.log(wordLetters);
+
+
+    // fill states
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
+
+    setGameStage(stages[1].name);
+  }, [pickWordAndCategory]);
+
+  //process the letter input
+  const verifyLetter = (letter) => {
+    //console.log(letter)
+
+    const normalizedLetter = letter.toLowerCase();
+
+    //check if letter has already been utilized
+    if(guessedLetters.includes(normalizedLetter) || 
+    wrongLetters.includes(normalizedLetter)
+    ) {
+      return;
+    }
+
+    //push guessed letter or remove a guess
+    if(letters.includes(normalizedLetter)) {
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters,
+         normalizedLetter
+      ]);
+    } else {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters,
+         normalizedLetter
+      ]);
+
+      setGuesses((actualGuesses) => actualGuesses - 1)
+
+    }
+
+    //console.log(guessedLetters)
+    //console.log(wrongLetters)
+
+  };
+
+  const clearLettersStates = () => {
+    setGuessedLetters([]);
+    setWrongLetters([]);
+  }
+  //check if guesses ended
+  useEffect(() => {
+    if (guesses <= 0) {
+      //reset all states
+      clearLettersStates();
+
+      setGameStage(stages[2].name);
+    }
+  }, [guesses]);
+
+  //check win condition
+  useEffect(() => {
+
+    const uniqueLetters = [...new Set(letters)];
+
+    //win condition
+    if(guessedLetters.length === uniqueLetters.length) {
+      //add score
+      setScore((actualScore) => actualScore += 100)
+
+      //restart game with new word
+      startGame();
+    }
+  }, [guessedLetters, letters, startGame]);
+
+  //resetar game
+  const retry = () => {
+    setScore(0);
+    setGuesses(guessesQty);
+
+
+    setGameStage(stages[0].name)
+  }
+
+    //console.log(words);
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {gameStage === 'start' && <StartScreen startGame={startGame} />}
+      {gameStage === 'game' && <Game 
+      verifyLetter={verifyLetter} 
+      pickedWord={pickedWord}
+      pickedCategory={pickedCategory}
+      letters={letters}
+      guessedLetters={guessedLetters}
+      wrongLetters={wrongLetters}
+      guesses={guesses}
+      score={score}
+      />}
+      {gameStage === 'end' && <GameOver retry={retry} score={score} />}
     </div>
   );
 }
